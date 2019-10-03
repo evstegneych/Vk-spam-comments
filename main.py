28a4231f97bdad2cfa379031ebfd6b08a9fef437
Evstegney <vavilon.maxim@mail.ru>
2019-10-03 23:15:37 +0300

    Имя паблика

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ text = input('')
 def core(name, api):
     global post
     global text
-    Logger.Glog(f'>>> Спамер запустил группу №{name}')
+    Logger.Glog(f'>>> Спамер запустил группу {name}')
     i = 1
     while True:
         try:
             api.wall.createComment(owner_id=post[0], post_id=post[1],
                                    message=text)
-            Logger.Pulselog(f'>>> Группа №{name} оставила коммент ({i})')
+            Logger.Pulselog(f'>>> Группа {name} оставила коммент ({i})')
             i += 1
             time.sleep(0.2)
         except vk_api.exceptions.ApiError as s:
@@ -40,14 +40,21 @@ def core(name, api):
             Logger.Rlog(f'Произошел сбой в {name} группе\n'
                         f'{s.__class__} {s}')
             time.sleep(60)
-    Logger.Rlog(f'Группа №{name} закончила свою работу')
+    Logger.Rlog(f'Группа {name} закончила свою работу')
 
 def main():
     for x in range(1, len(config['tokens']) + 1):
-        token = config['tokens'][x - 1]
-        vk_session = vk_api.VkApi(token=token)
-        vk = vk_session.get_api()
-        Thread(target=core, args=[x, vk]).start()
+        try:
+            token = config['tokens'][x - 1]
+            vk_session = vk_api.VkApi(token=token)
+            vk = vk_session.get_api()
+            name = vk.groups.getById()
+            name = name[0]['name']
+            Thread(target=core, args=[name, vk]).start()
+        except Exception as s:
+            Logger.Rlog(f'Произошел сбой в {name} группе\n'
+                        f'{s.__class__} {s}')
+
 
 
 if __name__ == '__main__':
